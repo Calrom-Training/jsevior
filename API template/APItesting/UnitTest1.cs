c552c62537017b64c9782b1028b6c0910456b9f1
William6613 <43117390+William6613@users.noreply.github.com>
2021-01-13 06:57:42 +0000

    unit test

---
+++ API template/APItesting/UnitTest1.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace APItesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public static bool Password_Checker(string fromUI, string fromDB)
+        {
+            bool result = false;
+            if (fromDB == fromUI)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        [TestMethod]
+        public void PasswordCheckerTestTrue()
+        {
+            string var1 = "testpassword";
+            string var2 = "testpassword";
+            bool result = Password_Checker(var1, var2);
+            Assert.IsTrue(result);
+        }
+    }
+}
